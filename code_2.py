49f85f611ed45c3a1b56245d21530a5294ccb3e9
LikaShu <142459648+LikaShu@users.noreply.github.com>
2023-11-14 21:42:35 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+
+}
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+    }
+    const data = generateRandomData();
+main();
+    for (let i = 0; i < 10; i++) {
+}
+
+    return data;
+function generateRandomData() {
+    const data = [];
+    });
+
+    data.forEach(item => {
